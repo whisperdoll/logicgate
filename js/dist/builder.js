0e2325ece1a18783f8093cc37756c4d5af099642
Brandon Kemp <brandon.kemp74@gmail.com>
2018-09-30 13:24:16 -0500

    remove dist folder

--- js/dist/builder.js
+++
@@ -1,7 +0,0 @@
-import { Canvas } from "./canvas";
-var Builder = (function () {
-    function Builder() {
-        this.canvas = new Canvas();
-    }
-    return Builder;
-}());
--- js/dist/input.js
+++
@@ -1,5 +0,0 @@
-var Input = (function () {
-    function Input() {
-    }
-    return Input;
-}());
